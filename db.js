b73cec98b253a96cf33bb4a18fedecfab3a958f1
Mili <muhamed.mili92@gmail.com>
2023-11-22 10:46:37 +0100

    rolback

--- db.js
+++ db.js
@@ -1,24 +1,36 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
-  {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+    'cloud_7z4q', // Database name
+    'cloud', // Username
+    'opJr86URM2bKNgPMC7h8aU2nfmsWOiAv', // Password
+    {
+        host: 'dpg-clesb7s15k1s73f5nv7g-a',
+        port: 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        define: {
+            createdAt: 'added',
+            updatedAt: 'updated',
+        }
     },
-    define: {
-      createdAt: 'added',
-      updatedAt: 'updated',
+);
+
+// Testing the connection
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connected to the database successfully.');
+        await sequelize.sync(); // This syncs your models with the database schema
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
     }
-  },
-)
+})();
 
-sequelize.authenticate()
-sequelize.sync()
-
-module.exports = sequelize
+module.exports = sequelize;
